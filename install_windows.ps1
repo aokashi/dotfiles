4c6756c2a7565cf021cc5e61fc46fd2f3a1e5ef3
Aokashi <Aokashi.Towa@Gmail.com>
2018-02-14 22:22:54 +0900

    gitconfig のシンボリックリンク作成コマンドを追加した

--- install_windows.ps1
+++ install_windows.ps1
@@ -1,4 +1,6 @@
-$path = "~\AppData\Roaming\Code\User"
+$vscode_path = "~\AppData\Roaming\Code\User"
+New-Item -ItemType SymbolicLink -Path $vscode_path -Name settings.json -Value .\vscode\settings.json
+New-Item -ItemType SymbolicLink -Path $vscode_path -Name snippets -Value .\vscode\snippets
 
-New-Item -ItemType SymbolicLink -Path $path -Name settings.json -Value .\vscode\settings.json
-New-Item -ItemType SymbolicLink -Path $path -Name snippets -Value .\vscode\snippets
+$home_path = "~"
+New-Item -ItemType SymbolicLink -Path $home_path -Name .gitconfig -Value .gitconfig
