3f0660a637cae096ab5292fcae6bf618b9deb0c7
Aokashi <Aokashi.Towa@Gmail.com>
2020-01-31 22:01:24 +0900

    インストール項目からopensshを削除、7zipをscoopに移行

--- installs/windows_scoop-install.ps1
+++ installs/windows_scoop-install.ps1
@@ -1,12 +1,21 @@
+# scoop をインストール
 # https://github.com/lukesampson/scoop
 iwr -useb get.scoop.sh | iex
 
 # 必要なパッケージをインストール
+# FIXME: このタイミングではまだ nvm の設定が終わっていないため、
+#     このまま yarn のインストールを進めると、 node が無いと怒られます。
+#     現在、対策方法を検討中です。
 scoop install `
+  7zip`
   git`
   graphviz`
   hugo-extended`
   mongodb`
   nvm`
-  openssh`
   yarn
+
+# あとは下記の作業も合わせて行ってください。
+# ・Windowsの設定から機能追加の画面に移り、 OpenSSH クライアントをインストール
+# ・Chocolatey をインストールし、 windows_chocolatey-packages.config を使用して各パッケージを追加
+# ・Microsoft Store から必要なソフトウェアを取得、インストール
